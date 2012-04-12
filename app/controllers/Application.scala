23d041be8a19aa44cffd4b48304bace25f4275c4
Julien Lafont <yotsumi.fx@gmail.com>
2012-04-12 21:42:24 +0200

    Remove JsRoutes

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -17,28 +17,13 @@ object Application extends Controller {
 		}
 	}
 	
+	/**
+	 * Return blank template (backbone will fetch the content)
+	 */
 	def blank(url: String) = Cached("blank") {
 		Action {
 			Ok(views.html.blank());
 		}
 	}
-	
-	def javascriptRoutes = Cached("jsRoutes") {
-		Action {
-			Ok(Routes.javascriptRouter("jsRoutes")(
-				controllers.api.routes.javascript.Finder.searchRepositories,
-				controllers.api.routes.javascript.Repository.commits,
-				controllers.api.routes.javascript.Repository.contributors,
-				controllers.api.routes.javascript.Repository.detail,
-				controllers.api.routes.javascript.Repository.languages,
-				controllers.api.routes.javascript.Repository.watchers,
-				controllers.api.routes.javascript.Stats.impact,
-				controllers.api.routes.javascript.Stats.languages,
-				controllers.api.routes.javascript.Stats.timeline,
-				controllers.api.routes.javascript.User.detail,
-				controllers.api.routes.javascript.User.geolocalisation,
-				controllers.api.routes.javascript.User.repos
-			)).as("text/javascript")
-		}
-	}
+
 }
